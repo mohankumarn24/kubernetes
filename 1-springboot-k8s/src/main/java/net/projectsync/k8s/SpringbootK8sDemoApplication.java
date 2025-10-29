50ad1a199435a04fcfd2c57f1b776aebf3410ceb
mohankumarn24 <mohankumar.n24@outlook.com>
2025-10-29 19:38:40 +0530

    use k8s deployment

---
+++ 1-springboot-k8s/src/main/java/net/projectsync/k8s/SpringbootK8sDemoApplication.java
@@ -0,0 +1,29 @@
+package net.projectsync.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootK8sDemoApplication {
+
+	@GetMapping({"/", "/welcome"})
+	public String welcome(){
+		return "Congratulations, you have successfully deployed your application to Kubernetes !!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootK8sDemoApplication.class, args);
+	}
+}
+
+
+/*
+ * https://www.youtube.com/watch?v=gMmcRbd8L5Y&list=PLVz2XdJiJQxybsyOxK7WFtteH42ayn5i9&index=11
+ * https://github.com/Java-Techie-jt/springboot-crud-k8s
+ * https://github.com/Java-Techie-jt/springboot-k8s-yaml
+ * 
+ * https://medium.com/@javatechie/kubernetes-tutorial-setup-kubernetes-in-windows-run-spring-boot-application-on-k8s-cluster-c6cab8f7de5a
+ */
---
+++ 3-springboot-crud-k8s/src/main/java/com/javatechie/crud/SpringbootCrudK8sExampleApplication.java
@@ -0,0 +1,21 @@
+package com.javatechie.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudK8sExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudK8sExampleApplication.class, args);
+	}
+
+}
+
+/*
+ * https://www.youtube.com/watch?v=gMmcRbd8L5Y&list=PLVz2XdJiJQxybsyOxK7WFtteH42ayn5i9&index=11
+ * https://github.com/Java-Techie-jt/springboot-crud-k8s
+ * https://github.com/Java-Techie-jt/springboot-k8s-yaml
+ * 
+ * https://medium.com/@javatechie/kubernetes-tutorial-setup-kubernetes-in-windows-run-spring-boot-application-on-k8s-cluster-c6cab8f7de5a
+ */
